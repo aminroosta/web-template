054defdf19b2343e79038dafc403b6507060e5c4
amin <amin.roosta@outlook.com>
2020-12-24 16:58:25 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import './index.scss';
-console.log('Hello :)');
+import React from 'react';
+import { render } from 'react-dom';
+import { Login } from './login';
+
+render(
+  React.createElement(Login, {}, null),
+  document.querySelector('#root')
+);
---
+++ src/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const BrushBackground = () => {
+  return <svg viewBox="0 0 366 299" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M234.084 198.165C155.533 241.558 -13.4296 260.512 -13.4296 260.512L-15.2757 173.621C-15.2757 173.621 74.797 169.977 154.818 136.005C234.839 102.033 292.996 6.07355 292.996 6.07355C292.996 6.07355 339.365 48.8537 335.59 96.3861C333.951 117.122 302.444 162.836 234.084 198.165Z" fill="#0CF2B4" />
+  </svg>
+}
+
+export const Login = () => {
+  return <>
+    <BrushBackground />
+    <h1>hello world!</h1>
+  </>
+}
